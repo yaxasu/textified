e0900b81aa5b62f4fc7ce00f4a5c33b17d47bf64
Neel <nishxpatel@gmail.com>
2024-05-22 15:43:56 -0400

    enhanced error handling

--- app/api/create-chat/route.ts
+++ app/api/create-chat/route.ts
@@ -1,9 +1,11 @@
 import { NextResponse, NextRequest} from "next/server"
 
-async function POST(req: NextRequest, res: NextResponse){
+export async function POST(req: NextRequest, res: NextResponse){
     try {
         const body = await req.json()
         const {file_key, file_name} = body
+        console.log(file_key, file_name)
+        return NextResponse.json({message: "Success"})
     } catch (error) {
         console.log(error)
         return NextResponse.json(
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-
-import {
-  ClerkProvider
-} from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs';
 import Providers from "@/components/ui/Providers";
+import { Toaster } from 'react-hot-toast';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,10 +21,12 @@ export default function RootLayout({
     <ClerkProvider>
       <Providers>
         <html lang="en">
-          <body className={inter.className}>{children}</body>
+          <body className={inter.className}>
+            {children}
+            <Toaster />
+          </body>
         </html>
       </Providers>
-
     </ClerkProvider>
   );
 }
