9e57f5289744a551a4cbe6dc441556146b6e5e71
Neel <nishxpatel@gmail.com>
2024-05-22 20:18:37 -0400

    fixed a getauth bug, and connected pinecone db

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -6,8 +6,10 @@ import React from 'react'
 import { useDropzone } from 'react-dropzone'
 import axios from 'axios'
 import toast from 'react-hot-toast'
+import {useRouter} from 'next/navigation'
 
 const FileUpload = () => {
+    const router = useRouter()
     const [uploading, setUploading] = React.useState(false)
     const [isLoading, setIsLoading] = React.useState(false)
 
@@ -46,12 +48,13 @@ const FileUpload = () => {
                     return
                 }
                 mutate(data, {
-                    onSuccess: (data) => {
-                        console.log(data)   
-                        //toast.success(data.message)
+                    onSuccess: ({chat_id}) => {
+                        toast.success("Chat created")
+                        router.push(`/chat/${chat_id}`)
                     },
                     onError: (error => {
                         toast.error("Error creating chat")
+                        console.log(error)
                     })
                 })
 
---
+++ lib/embeddings.ts
@@ -0,0 +1,21 @@
+import {OpenAIApi, Configuration} from 'openai-edge'
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(configuration)
+
+export async function getEmbeddings(text: string){  
+    try {
+        const response = await openai.createEmbedding({
+            model: 'text-embedding-3-small',
+            input: text.replace(/\n/g, ' ')
+        }) 
+        const result = await response.json()
+        return result.data[0].embedding as number[]
+    } catch (error) {
+        console.log("Error calling openai embeddings api", error)
+        throw error
+    }
+}
